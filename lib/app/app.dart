7a70028b6a6623b2df2d01f022ff8a37daa57846
Hector Morales <tuntu99@gmail.com>
2022-04-01 13:45:40 -0600

    Bloc pattern desing  implemented

---
+++ lib/app/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/tic_tac_toe/view/tic_tac_toe_view.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const TicTacToePage();
+  }
+}
---
+++ lib/tic_tac_toe/view/tic_tac_toe_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TicTacToePage extends StatelessWidget {
+  const TicTacToePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('Tic Tac Toe Pages'),
+    );
+  }
+}
