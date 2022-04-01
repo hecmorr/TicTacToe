75eedef2a7c8978110c3f64e882580789d4ed7b8
Hector Morales <tuntu99@gmail.com>
2022-04-01 14:30:26 -0600

    Showing the whole board

--- lib/tic_tac_toe/view/tic_tac_toe_page.dart
+++ lib/tic_tac_toe/view/tic_tac_toe_page.dart
@@ -38,23 +38,63 @@ class Board extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: 3,
-      physics: const ClampingScrollPhysics(),
-      children: [
-        Card(
-          color: MainColor.secondaryColor,
-          elevation: 5,
-          child: const InkWell(
-            child: Center(
-              child: Text(
-                ' - ',
-                style: TextStyle(fontSize: 40),
-              ),
+    return BlocBuilder<TicTacToeBloc, TicTacToeState>(
+      builder: (context, state) {
+        return AbsorbPointer(
+          absorbing: state.status != GameStatus.playing,
+          child: Container(
+            margin: const EdgeInsets.all(10),
+
+            ///Widget that builds the Board
+            child: GridView.count(
+              physics: const ClampingScrollPhysics(),
+              crossAxisCount: 3,
+              children: [
+                ///For that evaluates the value of each cell
+                for (var i = 0; i < state.board.length; i++)
+                  Builder(
+                    builder: (context) {
+                      final value = state.board[i];
+                      final isX = value < 0;
+                      final isO = value > 0;
+                      final text = () {
+                        if (isX) {
+                          return 'X';
+                        }
+                        if (isO) return 'O';
+                        return ' ';
+                      }();
+
+                      /// Widget that represents every cell
+                      return Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        color: MainColor.secondaryColor,
+                        elevation: 5,
+                        child: InkWell(
+                          onTap: () {
+                            context.read<TicTacToeBloc>().add(
+                                  TicTacToeCellClicked(i),
+                                );
+                          },
+                          child: Center(
+                            child: Text(
+                              text,
+                              style: TextStyle(
+                                color: isX ? MainColor.x : MainColor.o,
+                                fontSize: 40,
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+              ],
             ),
           ),
-        ),
-      ],
+        );
+      },
     );
   }
 }
