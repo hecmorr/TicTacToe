13a281dbc661487cdef867d452ca27bd1193b8a9
Hector Morales <tuntu99@gmail.com>
2022-04-01 14:37:17 -0600

    onTicTacToeCellClickked event added and working

--- lib/tic_tac_toe/bloc/tic_tac_toe_bloc.dart
+++ lib/tic_tac_toe/bloc/tic_tac_toe_bloc.dart
@@ -1,10 +1,26 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
-
 part 'tic_tac_toe_event.dart';
 part 'tic_tac_toe_state.dart';
 
 class TicTacToeBloc extends Bloc<TicTacToeEvent, TicTacToeState> {
-  TicTacToeBloc() : super(const TicTacToeState());
+  TicTacToeBloc() : super(const TicTacToeState()) {
+    on<TicTacToeCellClicked>(_onTicTacToeCellClicked);
+  }
+
+  /// X uses > 0 values and O uses < 0 values
+  void _onTicTacToeCellClicked(
+    TicTacToeCellClicked event,
+    Emitter<TicTacToeState> emit,
+  ) {
+    final newList = [...state.board];
+
+    final value = state.isXTurn ? 1 : -1;
+    newList[event.cellIndex] = value;
+    emit(
+      TicTacToeState(isXTurn: !state.isXTurn, board: newList),
+    );
+    add(TicTacToeWinnerVerified());
+  }
 }
